8c5eb1992d8487131ecf9f9d925f2066b775ca80
Drenzzz <realdrenzzz@gmail.com>
2025-01-31 16:34:41 +0000

    peridot: @ProjectMatrixx BringUp
    - Also inherit some common device flags.

--- lineage_peridot.mk
+++ lineage_peridot.mk
@@ -8,7 +8,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff.
+# Inherit some common Project Matrixx stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit from peridot device
@@ -16,6 +16,26 @@ $(call inherit-product, device/xiaomi/peridot/device.mk)
 
 # Inherit from the MiuiCamera setup
 $(call inherit-product-if-exists, device/xiaomi/peridot-miuicamera/device.mk)
+
+# Inherit some common device props
+TARGET_FACE_UNLOCK_SUPPORTED := true
+SYSTEM_OPTIMIZE_JAVA := true
+SYSTEMUI_OPTIMIZE_JAVA := true
+
+# Project Matrixx Flags
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_INCLUDE_STOCK_ARCORE := true
+TARGET_INCLUDE_STOCK_AICORE := true
+MATRIXX_BUILD_TYPE := Official
+
+# To Build Pixel launcher
+TARGET_INCLUDE_NEXUS := false
+
+# Gapps
+WITH_GMS := true
+
+# Bootanimation
+TARGET_BOOT_ANIMATION_RES := 1080
 
 PRODUCT_NAME := lineage_peridot
 PRODUCT_DEVICE := peridot
